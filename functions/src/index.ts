f4017dea9e70163693cca0b6e7994ed79ff88d96
JungWon Park <firstman9@protonmail.com>
2024-02-04 21:34:27 +0900

    functions

---
+++ functions/src/index.ts
@@ -0,0 +1,58 @@
+// functions/src/index.ts
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import express, {Request, Response} from "express";
+import cors from "cors";
+import {Todo} from "./models/todo";
+
+admin.initializeApp();
+
+const app = express();
+app.use(cors({ origin: true }));
+
+const db = admin.firestore();
+
+app.post("/todos/:userId/:familyId", async (req: Request, res: Response) => {
+  const { userId, familyId } = req.params;
+  const todo: Todo = {
+    userId,
+    familyId,
+    ...req.body,
+  };
+  const newDocRef = db.collection('todos').doc();
+  await newDocRef.set(todo);
+  res.status(201).send({ id: newDocRef.id, ...todo });
+});
+
+app.put("/todos/:userId/:familyId/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const updatedTodo: Partial<Todo> = req.body;
+  const docRef = db.collection('todos').doc(id);
+  await docRef.set(updatedTodo, { merge: true });
+  res.status(200).send({ id, ...updatedTodo });
+});
+
+app.get("/todos/:userId/:familyId", async (req: Request, res: Response) => {
+  const { userId, familyId } = req.params;
+  const snapshot = await db.collection('todos').where('userId', '==', userId).where('familyId', '==', familyId).get();
+  const todos: Todo[] = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() } as Todo));
+  res.status(200).json(todos);
+});
+
+app.get("/todos/:userId/:familyId/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const doc = await db.collection('todos').doc(id).get();
+  if (!doc.exists) {
+    res.status(404).send('Todo not found');
+  } else {
+    res.status(200).json({ id: doc.id, ...doc.data() } as Todo);
+  }
+});
+
+app.delete('/todos/:userId/:familyId/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  await db.collection('todos').doc(id).delete();
+  res.status(200).send();
+});
+
+exports.api = functions.https.onRequest(app);
---
+++ functions/src/models/todo.ts
@@ -0,0 +1,9 @@
+export interface Todo {
+	id?: string;
+	userId: string;
+	name: string;
+	title: string;
+	completed: boolean;
+	familyId: string;
+	description: string;
+}

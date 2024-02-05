82783a46f107c188ded3a984c87d47ee5307a530
JungWon Park <firstman9@protonmail.com>
2024-02-05 21:17:03 +0900

    funtions push

--- functions/src/models/todo.ts
+++ functions/src/models/todo.ts
@@ -1,9 +1,9 @@
 export interface Todo {
-	id?: string;
-	userId: string;
-	name: string;
-	title: string;
-	completed: boolean;
-	familyId: string;
-	description: string;
+  id?: string;
+  userId: string;
+  name: string;
+  title: string;
+  completed: boolean;
+  familyId: string;
+  description: string;
 }
